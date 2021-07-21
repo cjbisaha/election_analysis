eab8c15a587a76ca93f1cc38b99ce7426b36a2f6
CJ Bisaha <cjbisaha@gmail.com>
2021-07-20 21:37:20 -0400

    updated report and modified code

--- Resources/PyPoll_challenge_starter_code.py
+++ Resources/PyPoll_challenge_starter_code.py
@@ -18,8 +18,8 @@ candidate_options = []
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
-
-
+county_list = []
+county_votes = {}
 
 # Track the winning candidate, vote count and percentage
 winning_candidate = ""
@@ -27,7 +27,9 @@ winning_count = 0
 winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
-
+largest_county = ""
+largest_county_count = 0
+largest_county_percentage = 0
 
 
 # Read the csv and convert it into a list of dictionaries
@@ -47,7 +49,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-
+        county = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -64,15 +66,17 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-
+        if county not in county_list: 
 
             # 4b: Add the existing county to the list of counties.
-
+            county_list.append(county)
 
             # 4c: Begin tracking the county's vote count.
+            county_votes[county] = 0
 
 
         # 5: Add a vote to that county's vote count.
+        county_votes[county] += 1
 
 
 
@@ -91,34 +95,49 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
+    for county_name in county_votes:
 
         # 6b: Retrieve the county vote count.
+        vote_count = county_votes[county_name]
 
         # 6c: Calculate the percentage of votes for the county.
-
+        vote_percentage = float(vote_count) / float(total_votes) * 100
+        county_results = (
+            f"{county_name}: {vote_percentage:.1f}% ({vote_count:,}) \n")
 
          # 6d: Print the county results to the terminal.
+        print(county_results)
 
          # 6e: Save the county votes to a text file.
+        txt_file.write(county_results)
 
          # 6f: Write an if statement to determine the winning county and get its vote count.
-
+        # if (vote_count > winning_count) and (vote_percentage > winning_percentage):
+        if (vote_count > winning_count):
+            winning_count = vote_count
+            winning_county = county_name
+            winning_percentage = vote_percentage
 
     # 7: Print the county with the largest turnout to the terminal.
+    largest_county_summary = (
+        f"-------------------------\n"
+        f"Largest County Turnout: {winning_county}\n"
+        f"-------------------------\n")
+    print(largest_county_summary)
 
 
     # 8: Save the county with the largest turnout to a text file.
+    txt_file.write(largest_county_summary)
 
 
     # Save the final candidate vote count to the text file.
+    winning_count = 0
     for candidate_name in candidate_votes:
-
         # Retrieve vote count and percentage
         votes = candidate_votes.get(candidate_name)
         vote_percentage = float(votes) / float(total_votes) * 100
         candidate_results = (
             f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
         # Print each candidate's voter count and percentage to the
         # terminal.
         print(candidate_results)
@@ -126,7 +145,8 @@ with open(file_to_save, "w") as txt_file:
         txt_file.write(candidate_results)
 
         # Determine winning vote count, winning percentage, and candidate.
-        if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # if (votes > winning_count) and (vote_percentage > winning_percentage):
+        if (votes > winning_count):
             winning_count = votes
             winning_candidate = candidate_name
             winning_percentage = vote_percentage
